6111447c20f883a4cc269dba18d326b0cd013d9d
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2021-09-04 11:42:56 +0530

    Add files via upload

--- gooLE-ma-booglE.js
+++ gooLE-ma-booglE.js
@@ -1,4 +1,19 @@
+nosex = 0;
+nosey = 0;
+leftwristx = 0;
+rightwristx = 0;
+difference = 0;
 function setup(){
     canvas = createCanvas(550, 550);
-    canvas.position(1000, 0);
+    canvas.position(560, 150);
+    video = createCapture(VIDEO);
+    video.size(560, 500)
+    x = ml5.poseNet(video, gotposes);
+    x.on('pose', poses);
+}
+function gotposes(){
+    console.log("got poses from posenet library");
+}
+function poses(){
+
 }
