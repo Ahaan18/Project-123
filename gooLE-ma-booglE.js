05c3fb85da0668addca9175007d2798ea278a971
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2021-09-04 11:34:40 +0530

    Update gooLE-ma-booglE.js

--- gooLE-ma-booglE.js
+++ gooLE-ma-booglE.js
@@ -1,16 +1,4 @@
-nosex = 0;
-nosey = 0;
-leftwristx = 0;
-rightwristx = 0;
-difference = 0;
 function setup(){
     canvas = createCanvas(550, 550);
-    canvas.position(560, 150);
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    x = ml5.poseNet(video, gotposes);
-    x.on('pose', poses);
-}
-function gotposes(){
-    console.log("got poses from posenet library");
+    canvas.position(1000, 0);
 }
